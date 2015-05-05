9d5454ff9a53a7791fa557fb9c2a91277300999d
pohmelie <multisosnooley@gmail.com>
2015-05-05 15:50:37 +0300

    server permissions and users base

--- aioftp/server.py
+++ aioftp/server.py
@@ -1,26 +1,67 @@
 import asyncio
-import logging
 import pathlib
 import enum
 
 
-logger = logging.getLogger("aioftp")
+from . import common
 
 
 @enum.unique
-class DefaultUserPolitic(enum.Enum):
+class UserPolitic(enum.Enum):
 
     FORBIDDEN = 0
     ALLOWED = 1
 
 
+class Permission:
+
+    def __init__(self, path, *,
+                 readable=UserPolitic.ALLOWED,
+                 writable=UserPolitic.ALLOWED):
+
+        self.path = pathlib.Path(path)
+        self.readable = readable
+        self.writable = writable
+
+    def is_parent(self, other):
+
+        try:
+
+            other.relative_to(self.path)
+            return True
+
+        except ValueError:
+
+            return False
+
+    def __repr__(self):
+
+        return str.format(
+            "Permission({}, readable={}, writable={}",
+            self.path,
+            self.readable,
+            self.writable,
+        )
+
+
 class User:
 
-    def __init__(self, base_path, read_paths=(), write_paths=(),
-                 default_read_permission=DefaultUserPolitic.ALLOWED,
-                 default_write_permission=DefaultUserPolitic.ALLOWED):
+    def __init__(self, login=None, password=None, *,
+                 base_path=pathlib.Path("."), home_path=pathlib.Path("."),
+                 permissions=None):
 
-        pass
+        self.login = login
+        self.password = password
+        self.base_path = base_path
+        self.home_path = home_path
+        self.permissions = permissions or [Permission(".")]
+
+    def get_permissions(self, path):
+
+        path = pathlib.Path(path)
+        parents = filter(lambda p: p.is_parent(path), self.permissions)
+        perm = min(parents, key=lambda p: len(path.relative_to(p.path).parts))
+        return perm
 
 
 class Server:
