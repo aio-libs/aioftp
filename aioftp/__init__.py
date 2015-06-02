ab5066b8bbdc304c2e84ff6a8f087883f509960a
pohmelie <multisosnooley@gmail.com>
2015-06-02 16:56:42 +0300

    proper import

--- aioftp/__init__.py
+++ aioftp/__init__.py
@@ -2,8 +2,10 @@
 
 from .client import Client
 from .server import Server, User, Permission
-from .errors import *
+from .errors import StatusCodeError, ConnectionClosedError, \
+    UnknownPathType, PathIsNotAbsolute
 from .common import Code
+from .pathio import AbstractPathIO, PathIO, AsyncPathIO
 
 version = (0, 0, 1)
 __version__ = str.join(".", map(str, version))
