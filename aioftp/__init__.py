44454e110138c3e9af1ec6d523850f5623d0008b
pohmelie <multisosnooley@gmail.com>
2015-06-03 16:51:13 +0300

    MemoryPathIO and more common error for refusing connection

--- aioftp/__init__.py
+++ aioftp/__init__.py
@@ -2,10 +2,9 @@
 
 from .client import Client
 from .server import Server, User, Permission
-from .errors import StatusCodeError, ConnectionClosedError, \
-    UnknownPathType, PathIsNotAbsolute
+from .errors import StatusCodeError, PathIsNotFileOrDir, PathIsNotAbsolute
 from .common import Code
-from .pathio import AbstractPathIO, PathIO, AsyncPathIO
+from .pathio import AbstractPathIO, PathIO, AsyncPathIO, MemoryPathIO
 
 version = (0, 0, 1)
 __version__ = str.join(".", map(str, version))
