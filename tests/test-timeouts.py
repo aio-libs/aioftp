2e0f535d422f179a1cabda8e15237be59f102844
pohmelie <multisosnooley@gmail.com>
2015-06-04 17:13:52 +0300

    testing timeouts

---
+++ tests/test-timeouts.py
@@ -0,0 +1,53 @@
+import nose
+
+from common import *
+
+
+@nose.tools.raises(ConnectionResetError)
+@aioftp_setup(
+    server_args=(
+        [(aioftp.User(base_path="tests/foo", home_path="/"),)],
+        {"idle_timeout": 1}))
+@with_connection
+def test_idle_timeout(loop, client, server):
+
+    yield from asyncio.sleep(2, loop=loop)
+    yield from client.login()
+
+
+class SlowMemoryPathIO(aioftp.MemoryPathIO):
+
+    @asyncio.coroutine
+    def mkdir(self, path, parents=False):
+
+        yield from asyncio.sleep(10, loop=self.loop)
+
+
+@nose.tools.raises(ConnectionResetError)
+@aioftp_setup(
+    server_args=(
+        [(aioftp.User(base_path="tests/foo", home_path="/"),)],
+        {
+            "path_timeout": 1,
+            "path_io_factory": SlowMemoryPathIO,
+        }))
+@with_connection
+def test_path_timeout(loop, client, server):
+
+    print(server.path_io)
+    yield from client.login()
+    yield from client.make_directory("foo")
+
+
+if __name__ == "__main__":
+
+    import logging
+    import os
+
+    os.environ["AIOFTP_TESTS"] = "PathIO"
+    logging.basicConfig(
+        level=logging.INFO
+    )
+
+    test_path_timeout()
+    print("done")
