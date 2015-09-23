b969c70027d503055642a3e57df4695203e34570
pohmelie <multisosnooley@gmail.com>
2015-09-24 01:12:56 +0300

    added test for slow io + throttle and slowest io + throttle

--- tests/test-throttle.py
+++ tests/test-throttle.py
@@ -138,6 +138,81 @@ def test_client_write_throttle(loop, client, server, *, tmp_dir):
     big_file.unlink()
 
 
+class SlowPathIO(aioftp.PathIO):
+
+    @aioftp.with_timeout
+    @asyncio.coroutine
+    def write(self, fout, data):
+
+        timeout = len(data) / (100 * 1024)  # sleep as 100 Kib per second write
+        print("sleep for", timeout)
+        yield from asyncio.sleep(timeout, loop=self.loop)
+        yield from super().write(fout, data)
+
+
+@aioftp_setup(
+    client_args=(
+        [],
+        {
+            "write_speed_limit": 100 * 1024,  # 100 Kib per second
+            "path_io_factory": SlowPathIO,
+        },
+    ))
+@with_connection
+@with_tmp_dir("foo")
+def test_client_write_throttle_with_slow_io(loop, client, server, *, tmp_dir):
+
+    start = time.perf_counter()
+    big_file = tmp_dir / "foo.txt"
+    with big_file.open("wb") as fout:
+
+        fout.write(b"-" * 3 * 100 * 1024)
+
+    yield from client.login()
+    yield from client.download(
+        "tests/foo/foo.txt",
+        "tests/foo/bar.txt",
+        write_into=True
+    )
+
+    nose.tools.ok_(2.5 < (time.perf_counter() - start) < 3.5)
+    big_file.unlink()
+    received = tmp_dir / "bar.txt"
+    received.unlink()
+
+
+@aioftp_setup(
+    client_args=(
+        [],
+        {
+            "read_speed_limit": 200 * 1024,  # 200 Kib per second
+            "path_io_factory": SlowPathIO,
+        },
+    ))
+@with_connection
+@with_tmp_dir("foo")
+def test_client_read_throttle_with_too_slow_io(loop, client, server, *,
+                                               tmp_dir):
+
+    start = time.perf_counter()
+    big_file = tmp_dir / "foo.txt"
+    with big_file.open("wb") as fout:
+
+        fout.write(b"-" * 3 * 100 * 1024)
+
+    yield from client.login()
+    yield from client.download(
+        "tests/foo/foo.txt",
+        "tests/foo/bar.txt",
+        write_into=True
+    )
+
+    nose.tools.ok_(2.5 < (time.perf_counter() - start) < 3.5)
+    big_file.unlink()
+    received = tmp_dir / "bar.txt"
+    received.unlink()
+
+
 if __name__ == "__main__":
 
     import logging
