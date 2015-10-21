43d761927ea089a82d10c9652ef3784bd0c6751e
pohmelie <multisosnooley@gmail.com>
2015-10-21 13:02:15 +0300

    remove representation test

--- tests/test-user.py
+++ tests/test-user.py
@@ -9,15 +9,3 @@ from common import *  # noqa
 def test_user_not_absolute_home():
 
     aioftp.User(home_path="foo")
-
-
-def test_user_representation():
-
-    u = aioftp.User("foo", "bar", base_path=pathlib.PurePosixPath("."))
-    nose.tools.eq_(
-        repr(u),
-        "User('foo', 'bar', base_path=PurePosixPath('.'), "
-        "home_path=PurePosixPath('/'), permissions=" +
-        repr([aioftp.Permission()]) +
-        ", maximum_connections=" + repr(u.maximum_connections) + ")"
-    )
