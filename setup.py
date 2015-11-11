54d3e7427f38c796aec39dee84979ce4a572e6fa
pohmelie <multisosnooley@gmail.com>
2015-11-12 00:05:51 +0300

    remove flit from setup.py

--- setup.py
+++ setup.py
@@ -1,58 +1,72 @@
 import os
 import re
 from setuptools import setup, find_packages
-from configparser import ConfigParser
 from setuptools.command.test import test as TestCommand
-
-config = ConfigParser(default_section='metadata', empty_lines_in_values=False)
-config.read(os.path.join(os.path.dirname(__file__), 'flit.ini'))
 
 
 def read(f):
+
     return open(os.path.join(os.path.dirname(__file__), f)).read().strip()
 
 try:
+
     version = re.findall(r"""^__version__ = "([^']+)"\r?$""",
-                         read(os.path.join('aioftp', '__init__.py')), re.M)[0]
+                         read(os.path.join("aioftp", "__init__.py")), re.M)[0]
+
 except IndexError:
-    raise RuntimeError('Unable to determine version.')
+
+    raise RuntimeError("Unable to determine version.")
 
 
 class NoseTestCommand(TestCommand):
+
     def finalize_options(self):
+
         TestCommand.finalize_options(self)
         self.test_args = []
         self.test_suite = True
 
     def run_tests(self):
+
         import pathlib
         import shutil
         import nose
 
         for path in pathlib.Path("tests").glob("*"):
+
             if path.is_dir():
+
                 shutil.rmtree(str(path))
 
         cov = pathlib.Path(".coverage")
         if cov.exists():
+
             cov.unlink()
 
-        nose.run(argv=['nosetests'])
+        nose.run(argv=["nosetests"])
 
 
 setup(
-    name=config.defaults().get('module'),
+    name="aioftp",
     version=version,
-    description=('ftp client/server for asyncio'),
-    long_description=read('readme.rst'),
-    classifiers=config.defaults().get('classifiers').splitlines(),
-    author=config.defaults().get('author'),
-    author_email=config.defaults().get('author-email'),
-    url=config.defaults().get('home-page'),
-    license='Apache 2',
+    description=("ftp client/server for asyncio"),
+    long_description=read("readme.rst"),
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Topic :: Internet :: File Transfer Protocol (FTP)",
+    ],
+    author="pohmelie",
+    author_email="multisosnooley@gmail.com",
+    url="https://github.com/pohmelie/aioftp",
+    license="Apache 2",
     packages=find_packages(),
     install_requires=[],
-    tests_require=['nose', 'coverage'],
-    cmdclass={'test': NoseTestCommand},
+    tests_require=["nose", "coverage"],
+    cmdclass={"test": NoseTestCommand},
     include_package_data=True
 )
