16a6ea9da6015533d957c63546875b93a086c447
pohmelie <multisosnooley@gmail.com>
2015-12-19 18:31:19 +0300

    server: --memory argument for starting module as script

--- aioftp/__main__.py
+++ aioftp/__main__.py
@@ -6,6 +6,7 @@ Options:
     -q, --quiet             set logging level to "ERROR" instead of "INFO"
     --host=host             host for binding [default: 127.0.0.1]
     --port=port             port for binding [default: 8021]
+    --memory                use memory storage
 """
 import asyncio
 import logging
@@ -26,8 +27,17 @@ if not args["--quiet"]:
         datefmt="[%H:%M:%S]:",
     )
 
-user = aioftp.User(args["<login>"], args["<password>"])
-server = aioftp.Server([user])
+if args["--memory"]:
+
+    user = aioftp.User(args["<login>"], args["<password>"], base_path="/")
+    path_io_factory = aioftp.MemoryPathIO
+
+else:
+
+    user = aioftp.User(args["<login>"], args["<password>"])
+    path_io_factory = aioftp.AsyncPathIO
+
+server = aioftp.Server([user], path_io_factory=path_io_factory)
 
 loop = asyncio.get_event_loop()
 loop.run_until_complete(server.start(args["--host"], int(args["--port"])))
