78b2ce3357dbc0c3abae53d0bdda8c6a6d61202c
poh <multisosnooley@gmail.com>
2017-04-16 20:57:05 +0300

    version 0.7.0

--- aioftp/__init__.py
+++ aioftp/__init__.py
@@ -7,7 +7,7 @@ from .errors import *
 from .common import *
 from .pathio import *
 
-__version__ = "0.6.3"
+__version__ = "0.7.0"
 version = tuple(map(int, str.split(__version__, ".")))
 
 __all__ = (
