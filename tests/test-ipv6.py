a7470589a9ab11e6c414fd187104287e9d496e26
poh <multisosnooley@gmail.com>
2018-02-03 18:05:44 +0300

    tests: skip ipv6 test if `TRAVIS` in env

--- tests/test-ipv6.py
+++ tests/test-ipv6.py
@@ -1,10 +1,12 @@
 import socket
+import os
 
 from common import *
 
 
-@aioftp_setup()
-@with_connection(host="::1", family=socket.AF_INET6)
-async def test_ipv6_pasv_rejected(loop, client, server):
-    await client.login()
-    await client.list()
+if "TRAVIS" not in os.environ:
+    @aioftp_setup()
+    @with_connection(host="::1", family=socket.AF_INET6)
+    async def test_ipv6_list(loop, client, server):
+        await client.login()
+        await client.list()
