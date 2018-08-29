fda369b65a7337e408ba0e171aac1207a61005e8
Ted Kaplan <edward.kaplan@nielsen.com>
2018-08-29 16:52:02 -0700

    Add tests

--- tests/test-list-fallback.py
+++ tests/test-list-fallback.py
@@ -10,6 +10,9 @@ async def not_implemented(connection, rest):
     connection.response("502", ":P")
     return True
 
+async def implemented_badly(connection, rest):
+
+    nose.tools.ok_(False, 'should not be called')
 
 @aioftp_setup(server_args=([(aioftp.User(base_path="tests/foo"),)], {}))
 @with_connection
@@ -38,3 +41,24 @@ async def test_client_fallback_to_list_at_list(loop, client, server, *, tmp_dir)
     await client.remove("bar")
     files = await client.list()
     nose.tools.eq_(len(files), 0)
+
+@aioftp_setup(server_args=([(aioftp.User(base_path="tests/foo"),)], {}))
+@with_connection
+@with_tmp_dir("foo")
+async def test_client_list_override(loop, client, server, *, tmp_dir):
+
+    server.mlsd = implemented_badly
+
+    await client.login()
+    await client.make_directory("bar")
+
+    (path, stat), *_ = files = await client.list(raw_command="LIST")
+    nose.tools.eq_(len(files), 1)
+    nose.tools.eq_(path, pathlib.PurePosixPath("bar"))
+    nose.tools.eq_(stat["type"], "dir")
+
+@nose.tools.raises(ValueError)
+@aioftp_setup()
+@with_connection
+async def test_client_list_override_invalid_raw_command(loop, client, server):
+    await client.list(raw_command="FOO")
