4ba7910a1036a7523d813c47d6dad4468c02724f
Nikita Melentev <multisosnooley@gmail.com>
2018-08-30 02:53:28 +0300

    make flake8 happy

--- aioftp/client.py
+++ aioftp/client.py
@@ -610,7 +610,7 @@ class Client(BaseClient):
         :param recursive: list recursively
         :type recursive: :py:class:`bool`
 
-        :param raw_command optional ftp command to use in place of 
+        :param raw_command optional ftp command to use in place of
             fallback logic (must be one of "MLSD", "LIST")
         :type cmd :py:class:`str`
 
