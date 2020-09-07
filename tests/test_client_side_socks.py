5ba716f101fa3459d00d600663ce8f4b0467ccef
pohmelie <multisosnooley@gmail.com>
2020-09-07 21:33:26 +0300

    sync tests with new `siosocks` version (fixes #127)

--- tests/test_client_side_socks.py
+++ tests/test_client_side_socks.py
@@ -1,4 +1,5 @@
 import pytest
+from siosocks.exceptions import SocksException
 
 
 @pytest.mark.asyncio
@@ -13,6 +14,6 @@ async def test_socks_success(pair_factory, Client, socks):
 async def test_socks_fail(pair_factory, Client, socks):
     client = Client(socks_host=socks.host, socks_port=socks.port,
                     socks_version=5, username="bar", password="bar")
-    with pytest.raises(ValueError):
+    with pytest.raises(SocksException):
         async with pair_factory(client):
             pass
