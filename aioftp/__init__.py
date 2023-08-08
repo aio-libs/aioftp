49948399d574d396de8020698dbcedf16c0ffd0f
honglei <jhonglei@gmail.com>
2023-08-08 20:23:11 +0800

    fix mypy import error

--- aioftp/__init__.py
+++ aioftp/__init__.py
@@ -6,15 +6,21 @@ from .common import *
 from .errors import *
 from .pathio import *
 from .server import *
+from .client import __all__ as client_all
+from .server import __all__ as server_all
+from .errors import __all__ as errors_all
+from .common import __all__ as common_all
+from .pathio import __all__ as pathio_all
+
 
 __version__ = "0.21.4"
 version = tuple(map(int, __version__.split(".")))
 
 __all__ = (
-    client.__all__
-    + server.__all__
-    + errors.__all__
-    + common.__all__
-    + pathio.__all__
+    client_all
+    + server_all
+    + errors_all
+    + common_all
+    + pathio_all
     + ("version", "__version__")
 )
--- aioftp/client.py
+++ aioftp/client.py
@@ -31,7 +31,7 @@ from .common import (
 )
 
 try:
-    from siosocks.io.asyncio import open_connection
+    from siosocks.io.asyncio import open_connection # type: ignore
 except ImportError:
     from asyncio import open_connection
 
