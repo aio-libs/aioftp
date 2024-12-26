d5f8c97e5ed9776b871e1d2ff2bee140be574971
fti-akumari <159221208+fti-akumari@users.noreply.github.com>
2024-12-26 13:42:19 +0530

    Resolved Comments

--- src/aioftp/client.py
+++ src/aioftp/client.py
@@ -501,17 +501,17 @@ class BaseClient:
         date_time_str = line[: date_time_end + 1].strip().split(" ")
         date_time_str = " ".join([x for x in date_time_str if len(x) > 0])
         line = line[date_time_end + 1 :].lstrip()
-
-        possible_formats = ["%m/%d/%Y %I:%M %p", "%m-%d-%y %I:%M%p"]
+        
+        possible_formats = [
+        "%m/%d/%Y %I:%M %p",
+        "%m-%d-%y %I:%M%p",
+        ]
         date_time = None
-        with setlocale("C"):
+        with suppress(ValueError):
             for fmt in possible_formats:
-                try:
-                    date_time = datetime.datetime.strptime(date_time_str, fmt)
-                    break
-                except ValueError:
-                    continue
-            if not date_time:
+                date_time = datetime.datetime.strptime(date_time_str, fmt)
+                break
+            else:
                 raise ValueError(f"Date/time format not recognized: {date_time_str}")
 
         info = {}
